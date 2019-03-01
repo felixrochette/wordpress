94980c9998f9e846070b23262e9255172a44950d
felixrochette <44980809+felixrochette@users.noreply.github.com>
2019-03-01 16:40:19 -0500

    loop

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function(){
   let elmTitle =   document.querySelector(".site-branding .site-title");
   elmTitle.classList.add('animfelix')
+  let elmDescription =   document.querySelector(".site-branding .site-description");
+  elmDescription.classList.add('animdescription')
 })
